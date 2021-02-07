7c401a66055ef89624288a8caae06c12481578fe
David Robison <david@robisonweb.dev>
2021-02-07 10:03:07 -0700

    Created operate function that takes 2 numbers and a operator, then calls correct function to compute"

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function divide(num1, num2) {
         return num1 / num2;
     }
 }
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiple(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
