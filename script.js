4cc57f85aedcd2e53c2eb5121ef8d98c0afc511c
David Robison <david@robisonweb.dev>
2021-02-07 09:38:24 -0700

    Created divide function that accepts two values

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function subtract(num1, num2) {
 function multiple(num1, num2) {
     return num1 * num2;
 }
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return `Can't divide by 0`;
+    } else {
+        return num1 / num2;
+    }
+}
